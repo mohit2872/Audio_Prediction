67cdb4dade9394b5907874bc8a2e61a383eb7e2b
Mohit Gupta <mg909@snu.edu.in>
2018-10-25 20:46:03 +0530

    done with dct

--- Reference.md
+++ Reference.md
@@ -3,6 +3,8 @@ https://librosa.github.io/librosa/onset.html
 
 For feature extraction we will look into python speech features
 https://python-speech-features.readthedocs.io/en/latest/
+	For further understanding of MFCC go to the link:
+	http://www.practicalcryptography.com/miscellaneous/machine-learning/guide-mel-frequency-cepstral-coefficients-mfccs/
 
 For incremental clustering we will use Cobweb (not HDP-HMM as it is offline), taking help from the concept-formation library:
 https://concept-formation.readthedocs.io/en/latest/examples.html
--- Tutorials/segmentation.py
+++ Tutorials/segmentation.py
@@ -4,6 +4,8 @@ import librosa
 from python_speech_features import mfcc
 import scipy.io.wavfile as wav
 from pydub import AudioSegment
+from numpy import matrix
+from scipy.fftpack import dct
 
 # Loading the audio from 30 to 35 secs in the 'y'
 y, sr = librosa.load("./Tutorials/sample.wav", offset=30, duration=5.0)
@@ -19,5 +21,12 @@ newAudio.export('./Tutorials/newSample.wav', format="wav")
 
 # Getting the MFCC
 (rate,sig) = wav.read("./Tutorials/newSample.wav")
-# we want 13*4 ,hence we are taking out all the further rows
-mfcc_feat = mfcc(signal=sig, samplerate=rate, nfft=2048, ceplifter=4)[:4, :]
+mfcc_feat = matrix.transpose(mfcc(signal=sig, samplerate=rate, nfft=2048, ceplifter=4))
+
+# for getting the first 4 dct of the mfcc we obtained
+# TODO: In paper it said that we get 52 dimensional vector. Check that???
+mfcc_feat_dct = []
+for mfcc_coeff in mfcc_feat:
+    mfcc_feat_dct.append(dct(x=mfcc_coeff, n=4))
+
+    
